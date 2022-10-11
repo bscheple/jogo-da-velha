48c26c822447cc660d48c57706121136ed7c61b5
bscheple <bscheple@gmail.com>
2022-10-11 14:53:44 -0300

    consertos

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let jogador = "X";
 let casa01 = document.getElementById("casa01");
 let casa02 = document.getElementById("casa02");
 let casa03 = document.getElementById("casa03");
+let casa04 = document.getElementById("casa04");
+let casa05 = document.getElementById("casa05");
+let casa06 = document.getElementById("casa06");
+let casa07 = document.getElementById("casa07");
+let casa08 = document.getElementById("casa08");
+let casa09 = document.getElementById("casa09");
 
 
 function jogada(casa) {
@@ -23,7 +29,28 @@ function alterna_jogador() {
     }
 }
 function verifica_ganhador() {
-    if (casa01.innerHTML!="" && casa01.innerHTML===casa02.innerHTML && casa01.innerHTML=== casa03.innerHTML){
+    if (casa01.innerHTML!="" && casa01.innerHTML===casa02.innerHTML && casa02.innerHTML=== casa03.innerHTML){
         alert(`Temos uma ganhador: ${casa01.innerHTML}`)
+    }
+    if (casa04.innerHTML!="" && casa04.innerHTML===casa05.innerHTML && casa05.innerHTML=== casa06.innerHTML){
+        alert(`Temos uma ganhador: ${casa04.innerHTML}`)
+    }
+    if (casa07.innerHTML!="" && casa07.innerHTML===casa08.innerHTML && casa07.innerHTML=== casa09.innerHTML){
+        alert(`Temos uma ganhador: ${casa07.innerHTML}`)
+    }
+    if (casa01.innerHTML!="" && casa01.innerHTML===casa05.innerHTML && casa01.innerHTML=== casa09.innerHTML){
+        alert(`Temos uma ganhador: ${casa01.innerHTML}`)
+    }
+    if (casa03.innerHTML!="" && casa03.innerHTML===casa05.innerHTML && casa03.innerHTML=== casa07.innerHTML){
+        alert(`Temos uma ganhador: ${casa03.innerHTML}`)
+    }
+    if (casa01.innerHTML!="" && casa01.innerHTML===casa04.innerHTML && casa01.innerHTML=== casa07.innerHTML){
+        alert(`Temos uma ganhador: ${casa01.innerHTML}`)
+    }
+    if (casa02.innerHTML!="" && casa02.innerHTML===casa05.innerHTML && casa02.innerHTML=== casa08.innerHTML){
+        alert(`Temos uma ganhador: ${casa02.innerHTML}`)
+    }
+    if (casa03.innerHTML!="" && casa03.innerHTML===casa06.innerHTML && casa03.innerHTML=== casa09.innerHTML){
+        alert(`Temos uma ganhador: ${casa03.innerHTML}`)
     }
 }
